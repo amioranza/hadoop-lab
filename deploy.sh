30d985b4949a870c8469b3d28c6a09afb0d7e10a
A. Mioranza <amioranza@mdcnet.ninja>
2019-01-30 23:32:01 -0200

    First commit

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+HDP_VERSION="3.2.0"
+HDP_HOME="/opt/hadoop-${HDP_VERSION}"
+
+echo "### Installing Hadoop pre-reqs ###"
+sudo apt-get update && apt-get upgrade -y
+sudo apt-get install -y openjdk-8-jdk ssh rsync
+
+echo "### Creating users and groups ###"
+sudo addgroup hadoop
+sudo adduser --system --quiet --ingroup hadoop hduser
+
+echo "### Extracting Hadoop ###"
+sudo cp -v /vagrant/hadoop-${HDP_VERSION}.tar.gz /opt
+cd /opt
+sudo tar -xzvf hadoop-${HDP_VERSION}.tar.gz 
+
+echo "### Copying initial setup files ###"
+sudo cp -v /vagrant/etc/* /opt/hadoop-${HDP_VERSION}/etc/hadoop/
+
+echo "### Set profile to export Hadoop vars"
+sudo cp -v /vagrant/profile/* /etc/profile.d/
+source /etc/profile.d/Z99-hadoop-vars.sh
+
+echo "### Format the NameNode ###"
+cd ${HDP_HOME}
+bin/hdfs namenode -format
+
+echo "# Starting server (DFS,YARN)"
+cd ${HDP_HOME}
+sbin/start-dfs.sh
+sbin/start-yarn.sh
---
+++ profile/Z99-hadoop-vars.sh
@@ -0,0 +1,9 @@
+# Hadoop Environment
+export HADOOP_HOME="/opt/hadoop-3.2.0/"
+export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$HADOOP_HOME/sbin
+export HDFS_NAMENODE_USER="root"
+export HDFS_DATANODE_USER="root"
+export HDFS_SECONDARYNAMENODE_USER="root"
+export YARN_RESOURCEMANAGER_USER="root"
+export YARN_NODEMANAGER_USER="root"
